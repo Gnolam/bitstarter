2bf185228c42413c7cb4173a328dca3f0446e82a
Ubuntu <ubuntu@ip-10-253-95-153.us-west-2.compute.internal>
2013-07-23 14:41:24 +0000

    Updated listener

--- web.js
+++ web.js
@@ -1,16 +1,17 @@
+#!/usr/bin/env node
 var fs = require('fs');
 var express = require('express');
+var htmlfile="index.html";
 
 var app = express.createServer(express.logger());
-var buf = fs.readFileSync('index.html');
 
 
 app.get('/', function(request, response) {
-  var buf= fs.readFileSync('index.html');
-  response.send(buf.toString());
+  var html= fs.readFileSync("index.html").toString();
+  response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
